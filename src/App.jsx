8ea030bc1c9a08ebe45b76c16ec0e5fa90eaa15b
StormbadeM <sodiqmakanjuola1@gmail.com>
2026-01-23 06:48:51 +0100

    ook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { ProductProvider } from "./contexts/useProductContext";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Applayout from "./ui/Applayout";
-import ProductItem from "./ui/ProductItem";
+
 import CartPage from "./pages/CartPage";
 import WishlistPage from "./pages/WishlistPage";
+import ProductDetails from "./features/menu/ProductDetails";
 const BASE_URL = "http://localhost:8000/";
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Applayout />} />
-          <Route path="/:product/:id" element={<ProductItem />} />
+          <Route path="/:product/:id" element={<ProductDetails />} />
           <Route path="/cart" element={<CartPage />} />
           <Route path="/wishlist" element={<WishlistPage />} />
         </Routes>

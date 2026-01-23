6bbd36310d78ae2ad8000bdb58883fe1fc482eed
Nice&Guy <119658625+stormbladeM@users.noreply.github.com>
2026-01-23 06:51:46 +0100

    fix: Resolve merge conflicts and use correct components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
 import { ProductProvider } from "./contexts/useProductContext";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Applayout from "./ui/Applayout";
-
+import Home from "./ui/Home";
+import ProductDetails from "./features/menu/ProductDetails";
 import CartPage from "./pages/CartPage";
 import WishlistPage from "./pages/WishlistPage";
-<<<<<<< HEAD
-import ProductDetails from "./features/menu/ProductDetails";
-=======
-
->>>>>>> a81894eb26f61ae63358ad6268d308eafd15b31c
-const BASE_URL = "http://localhost:8000/";
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Applayout />,
+    errorElement: <Error />,
     children: [
       {
         index: true,
-        element: <div>Home Content Here</div>, // You can replace this with your home component
+        element: <Home />,
       },
       {
         path: ":product/:id",
-        element: <ProductItem />,
+        element: <ProductDetails />,
       },
       {
         path: "cart",
@@ -39,18 +36,7 @@ const router = createBrowserRouter([
 function App() {
   return (
     <ProductProvider>
-<<<<<<< HEAD
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Applayout />} />
-          <Route path="/:product/:id" element={<ProductDetails />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/wishlist" element={<WishlistPage />} />
-        </Routes>
-      </BrowserRouter>
-=======
       <RouterProvider router={router} />
->>>>>>> a81894eb26f61ae63358ad6268d308eafd15b31c
     </ProductProvider>
   );
 }

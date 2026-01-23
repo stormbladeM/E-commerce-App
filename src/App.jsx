a81894eb26f61ae63358ad6268d308eafd15b31c
Nice&Guy <119658625+stormbladeM@users.noreply.github.com>
2026-01-23 06:46:36 +0100

    fix: Switch to createBrowserRouter for Data Mode to support useNavigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
 import { ProductProvider } from "./contexts/useProductContext";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Applayout from "./ui/Applayout";
 import ProductItem from "./ui/ProductItem";
 import CartPage from "./pages/CartPage";
 import WishlistPage from "./pages/WishlistPage";
+
 const BASE_URL = "http://localhost:8000/";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Applayout />,
+    children: [
+      {
+        index: true,
+        element: <div>Home Content Here</div>, // You can replace this with your home component
+      },
+      {
+        path: ":product/:id",
+        element: <ProductItem />,
+      },
+      {
+        path: "cart",
+        element: <CartPage />,
+      },
+      {
+        path: "wishlist",
+        element: <WishlistPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <ProductProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Applayout />} />
-          <Route path="/:product/:id" element={<ProductItem />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/wishlist" element={<WishlistPage />} />
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </ProductProvider>
   );
 }
